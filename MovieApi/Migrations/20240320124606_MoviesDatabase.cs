4be7d705321e4018be5c89307fb996eae7ae7844
Thiago Augusto <taugusto964@gmail.com>
2024-03-20 09:52:51 -0300

    Migration
    Migration e criaçao do dabtabase

---
+++ MovieApi/Migrations/20240320124606_MoviesDatabase.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class MoviesDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Diretor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Sinopse = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    Duracao = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
--- MovieApi/Models/Movie.cs
+++ MovieApi/Models/Movie.cs
@@ -16,7 +16,7 @@ namespace MovieApi.Models
         [StringLength(200,ErrorMessage ="Máximo de 200 caracteres!")]
         public string Sinopse { get; set; }
 
-        [Range(1,600,ErrorMessage ="O campo duração deve ter no mínimo 1 e máximo 600 caracteres!")]
+        [Range(1,240,ErrorMessage ="O campo duração deve ter no mínimo 1 e máximo 240 minutos!")]
         [Display(Name ="Duração")]
         public int Duracao {  get; set; }
 
