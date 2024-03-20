f9463f5f2b7afaab5de8b86ce8cab05047898639
Thiago Augusto <taugusto964@gmail.com>
2024-03-20 09:33:11 -0300

    Classes de modelo
    Adicionadas as classes de filme e do contexto do banco de dados

---
+++ MovieApi/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApi.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Obrigatório informar o título!")]
+        [StringLength(50)]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage ="Obrigatório informar o diretor!")]
+        public string Diretor {  get; set; }
+
+        [StringLength(200,ErrorMessage ="Máximo de 200 caracteres!")]
+        public string Sinopse { get; set; }
+
+        [Range(1,600,ErrorMessage ="O campo duração deve ter no mínimo 1 e máximo 600 caracteres!")]
+        [Display(Name ="Duração")]
+        public int Duracao {  get; set; }
+
+    }
+}
---
+++ MovieApi/Models/MovieContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieApi.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
