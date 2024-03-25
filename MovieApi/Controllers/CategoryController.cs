c44c17bbc1f731cae47c3107bdc17be3c17c107f
Thiago Augusto <taugusto964@gmail.com>
2024-03-25 16:19:47 -0300

    Criacao dos controllers

---
+++ MovieApi/Controllers/CategoryController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieApi.Models;
+using MovieApi.Repositories.Interfaces;
+
+namespace MovieApi.Controllers
+{
+    public class CategoryController : ControllerBase
+    {
+        private readonly IBaseRepository<Category> _repository;
+
+        public CategoryController(IBaseRepository<Category> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        // GET: Category
+        public ActionResult GetAll()
+        {
+            var categorias = _repository.GetAll();
+            return Ok(categorias);
+        }
+
+        [HttpGet("{id}")]
+        // GET: Category/{id}
+        public ActionResult Get(int id)
+        {
+            var categoriaExistente = _repository.Get(c=>c.Id ==  id);
+            if(categoriaExistente == null)
+                return NotFound(); 
+            return Ok(categoriaExistente);
+        }
+
+
+       
+        // POST: Category
+        [HttpPost]
+        public IActionResult Create(Category categoria)
+        {
+            var categoriaCriada = _repository.Create(categoria);
+            return new CreatedAtActionResult(nameof(Get), "Category",
+            new { id = categoriaCriada.Id },
+            categoriaCriada);
+        }
+
+       
+        // PUT: Category/{id}
+        [HttpPut("{id}")]
+        public ActionResult Edit(int id, Category categoria)
+        {
+            if(id!=categoria.Id)
+                return BadRequest();
+            var categoriaExistente = _repository.Get(c=>c.Id==id);
+
+            if (categoriaExistente == null)
+                return NotFound();
+
+            var categoriaAtualizada = _repository.Update(categoriaExistente);
+            return Ok(categoriaAtualizada);
+        }
+
+        // DELETE: Category/{id}
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var categoriaExistente = _repository.Get(c => c.Id == id);
+            if(categoriaExistente == null)
+                return NotFound();
+
+            var categoriaExcluida = _repository.Delete(categoriaExistente);
+            return  Ok(categoriaExcluida);
+
+        }
+    }
+}
---
+++ MovieApi/Controllers/MovieController.cs
@@ -0,0 +1,92 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieApi.Models;
+using MovieApi.Repositories.Interfaces;
+
+namespace MovieApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private readonly IBaseRepository<Movie> _repository;
+        private readonly IMovieRepository _movieRepository;
+
+        public MovieController(IMovieRepository movieRepository,IBaseRepository<Movie>repository)
+        {
+            _repository = repository;
+            _movieRepository = movieRepository;
+        }
+
+        //GET: 
+        [HttpGet("Movie/{id}")]
+        public ActionResult<IEnumerable<Movie>> GetMoviesByCategory(int id)
+        {
+            var filmesPorCategoria = _movieRepository.GetMoviesByCategory(id);
+
+            if (filmesPorCategoria == null)
+                return NotFound();
+          return Ok(filmesPorCategoria);
+        }
+
+        // GET: Movie
+        [HttpGet]
+        public ActionResult<IEnumerable<Movie>> GetAll()
+        {
+            var filmes = _repository.GetAll();
+            return Ok(filmes);
+        }
+
+        // GET: Movie/{id}
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            var filmeExistente = _repository.Get(m=>m.Id == id);
+
+            if(filmeExistente == null)
+                return NotFound();
+
+            return Ok(filmeExistente);
+
+        }
+
+        
+
+        // POST: Movie
+        [HttpPost]
+        public IActionResult Create(Movie filme)
+        {
+            var filmeCriado = _repository.Create(filme);
+            return new CreatedAtActionResult(nameof(Get), "Movie", new {id = filmeCriado.Id},filmeCriado);
+        }
+
+
+        // PUT: Movie
+        [HttpPost]
+       
+        public ActionResult Edit(int id, Movie filme)
+        {
+           if(id!= filme.Id)
+                return BadRequest();
+           var filmeExistente = _repository.Get(f=> f.Id == id);
+            if(filmeExistente == null)
+                return NotFound();
+            var filmeAtualizado = _repository.Update(filmeExistente);
+            return Ok(filmeAtualizado);
+        
+        }
+
+        
+       
+        // POST: Movie/{id}
+        public ActionResult Delete(int id)
+        {
+            var filmeExistente = _repository.Get(f => f.Id == id);
+            if (filmeExistente == null)
+                return NotFound();
+            var filmeExcluido = _repository.Delete(filmeExistente);
+            return Ok(filmeExcluido);
+        }
+    }
+}
