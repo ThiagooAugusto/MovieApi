fc57513f26c3ff64c7e21f21a042c7b83131bcd7
Thiago Augusto <taugusto964@gmail.com>
2024-03-25 17:32:18 -0300

    Criadas Migrations
    Tabelas Movies e Categories populadas através de migrations

---
+++ MovieApi/Migrations/20240325194843_PopulaCategorias.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categories(Nome) VALUES('Ficção Científica')");
+            migrationBuilder.Sql("INSERT INTO Categories(Nome) VALUES('Guerra')"); 
+            migrationBuilder.Sql("INSERT INTO Categories(Nome) VALUES('Romance')");
+            migrationBuilder.Sql("INSERT INTO Categories(Nome) VALUES('Terror')");
+            migrationBuilder.Sql("INSERT INTO Categories(Nome) VALUES('Animação')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categories");
+        }
+    }
+}
---
+++ MovieApi/Migrations/20240325195923_PopulaFilmes.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaFilmes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Movies(Titulo,Diretor,Sinopse,Duracao,CategoryId) VALUES('A Origem','Christopher Nolan','Em A Origem, Dom Cobb é um especialista em roubo de segredos corporativos através do uso de tecnologia de compartilhamento de sonhos.',148,1)");
+            migrationBuilder.Sql("INSERT INTO Movies(Titulo,Diretor,Sinopse,Duracao,CategoryId) VALUES('Resgate do Soldado Ryan','Steve Spielberg','Durante a Segunda Guerra Mundial, um grupo de soldados americanos é enviado para trás das linhas inimigas para resgatar um soldado para casa depois que seus três irmãos são mortos em combate.',169,2)");
+            migrationBuilder.Sql("INSERT INTO Movies(Titulo,Diretor,Sinopse,Duracao,CategoryId) VALUES('Diário de uma Paixão','Nick Cassavetes','o filme conta a história de um casal apaixonado, Noah e Allie, que são separados pelo destino e pelas circunstâncias sociais.',149,3)");
+            migrationBuilder.Sql("INSERT INTO Movies(Titulo,Diretor,Sinopse,Duracao,CategoryId) VALUES('O Iluminado','Stanley Kubrick','O filme segue a história de Jack Torrance, um escritor e alcoólatra que aceita um emprego como zelador de um hotel isolado nas montanhas.',144,4)");
+            migrationBuilder.Sql("INSERT INTO Movies(Titulo,Diretor,Sinopse,Duracao,CategoryId) VALUES('Toy Story','John Lasseter',' primeiro filme da trilogia Toy Story segue a vida de brinquedos que ganham vida quando seus donos não estão por perto.',81,5)");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Movies");
+        }
+    }
+}
